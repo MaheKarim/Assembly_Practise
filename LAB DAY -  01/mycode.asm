0e671166155ada45aca5fa8032360b624bc6cd84
MaheKarim <mahekarim@gmail.com>
2019-05-16 01:08:12 +0600

    Lab Day 01 - Practise

--- LAB DAY -  01/mycode.asm
+++ LAB DAY -  01/mycode.asm
@@ -1,34 +1,37 @@
-    ;CODE by Mahe Karim
-   
-   .MODEL SMALL
-   .STACK 100H
+                                    ;CODE by Mahe Karim
+                                    ;Date: 16 May 2019
+
+   .MODEL SMALL 
+   .STACK 100H                      ; stack size 100 hexadecimal
    .DATA
         MSG DB "HELLO WORLD! $"
-                                     ; the $ sign means end OF the string
-                                     ; MSG just a variable
-    .CODE                            ; code segmentation start 
+                                    ; the $ sign means end OF the string
+                                    ; MSG just a variable
+
+    .CODE                           ; code segmentation start 
     MAIN PROC                       ; procedure start from here
 
-    MOV AX, DATA
+    MOV AX, DATA                    ; address data to ax
     MOV DS, AX                      ; initialize DS 
 
     ; from here we start display message actually msg variable
 
     LEA DX, MSG                     ; get message
-    MOV AH, 9                       ; display string function
+    MOV AH, 9                       ; AH, 9 is a string display function
     INT 21H                         ; display that msg  and executing 
 
     ; return to DOS 
 
     MOV AH, 4CH
-    INT 21H
+    INT 21H                         ; exit from DOS / exit()
 
-    MAIN ENDP
-    ENDP
+    MAIN ENDP                       ; main func (main proc) end 
+    ENDP                            ; program end
 
 
-    
-;    LEA means 
+
+
+;    LEA means Load Effective Address. It's puts the copy of the source address to destination
 ;    DATA => is defined by data segment
 
 
