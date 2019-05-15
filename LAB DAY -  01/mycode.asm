06a6e3e4dd4d7b690303f52ff793098d8768e2ea
MaheKarim <mahekarim@gmail.com>
2019-05-16 00:57:40 +0600

    Lab Day  - 01  /  Practise

--- LAB DAY -  01/mycode.asm
+++ LAB DAY -  01/mycode.asm
@@ -1,16 +1,51 @@
+    ;CODE by Mahe Karim
+   
    .MODEL SMALL
    .STACK 100H
    .DATA
-        MSG DB "HELLO WORLD! "
+        MSG DB "HELLO WORLD! $"
+                                     ; the $ sign means end OF the string
+                                     ; MSG just a variable
 
-    .CODE
+
+    .CODE                            ; code segmentation start 
+
+     
+    MAIN PROC                       ; procedure start from here
+
 
     MOV AX, DATA
-    MOV DS, AX
+    MOV DS, AX                      ; initialize DS 
 
 
-    LEA DX, MSG
-    MOV AH, 09H
-    INT21H
+    ; from here we start display message actually msg variable
 
-    END 
+
+    LEA DX, MSG                     ; get message
+    MOV AH, 9                       ; display string function
+    INT 21H                         ; display that msg  and executing 
+
+    ; return to DOS 
+
+
+    MOV AH, 4CH
+    INT 21H
+
+
+    MAIN ENDP
+    ENDP
+
+
+    
+;    LEA means 
+;    DATA => is defined by data segment
+;
+;
+;
+;
+;
+;
+;
+;
+
+
