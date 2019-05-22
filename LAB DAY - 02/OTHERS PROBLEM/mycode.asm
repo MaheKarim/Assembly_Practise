11e4465b61859cfd56a4bded65a5a8e2fbc2d2eb
MaheKarim <mahekarim@gmail.com>
2019-05-22 23:44:55 +0600

    Full Comment Out

--- LAB DAY - 02/OTHERS PROBLEM/mycode.asm
+++ LAB DAY - 02/OTHERS PROBLEM/mycode.asm
@@ -10,25 +10,25 @@ INCLUDE "EMU8086.INC"                     ; emu8086 helper added
     PRINT "ENTER YOUR FIRST VALUE: "
     MOV AH, 1
     INT 21H                               ; call the interrupt handler 0x21
-    MOV BH, AL                            ; first value store
+    MOV BH, AL                            ; first value store BH
     
     PRINTN
     PRINT "ENTER YOUR SECONED VALUE: "
     INT 21H
-    MOV BL, AL                            ; seconed value store
+    MOV BL, AL                            ; seconed value store BL
     
     PRINTN
     PRINT "ENTER YOUR THIRD VALUE: "   
     INT 21H
-    MOV CH, AL                             ; third value store
+    MOV CH, AL                             ; third value store CH 
     
 
     ADD BH, BL                             ; add functionality
-    SUB BH, 30H
+    SUB BH, 30H                            ; sub assembler value when we do add
     
 
     SUB BH, CH                             ; subtract functionality
-    ADD BH, 30H
+    ADD BH, 30H                            ; add assembler value when we do sub
     
     
     PRINTN                                 ; new line printer
