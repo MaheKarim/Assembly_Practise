f7a89e85e7a78ee8b76ba3b3e5639854b274fa8e
MaheKarim <mahekarim@gmail.com>
2019-05-23 00:43:52 +0600

    Beep Code Added

--- _Extra Practise/Beep/mycode.asm
+++ _Extra Practise/Beep/mycode.asm
@@ -1,2 +1,17 @@
 INCLUDE "EMU8086.INC"
 
+.MODEL SMALL
+.STACK 100H
+.CODE 
+
+         MAIN PROC
+
+
+                                
+         MOV DL, 007                        ; PRINT Dl REG AND BEEP CODE ASCII VALUE
+         MOV AH, 2                          ; PRINT OUTPUT
+         INT 21H                            ; INTERUPT DOS
+
+
+         MAIN ENDP                           ; END PROGRAME
+         END MAIN
