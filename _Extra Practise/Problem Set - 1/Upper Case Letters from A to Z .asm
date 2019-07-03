f9e9b4fc0d342f02021eaf21a7d1cce788f699ce
MaheKarim <mahekarim@gmail.com>
2019-07-03 11:45:04 +0600

    Upper Case Letters from A to Z

---
+++ _Extra Practise/Problem Set - 1/Upper Case Letters from A to Z .asm
@@ -0,0 +1,19 @@
+;Program to print Upper Case Letters from A to Z  
+
+INCLUDE "emu8086.inc"
+.MODEL SMALL
+.STACK 100H
+.CODE
+MAIN PROC
+    
+    MOV CX,26
+    MOV AH,2
+    MOV DL,"A"
+    
+    LEVEL:
+    INT 21H
+    INC DL
+    LOOP LEVEL
+    
+    MAIN ENDP
+END MAIN
